699bad681f29a70c438ab553e6fd5c10df3a583f
Hadar Pinhas <hadarpinhas@gmail.com>
2023-12-07 00:09:39 +0200

    detection with several match templates per frame

--- VideoMotionDetector/ObjectDetection.py
+++ VideoMotionDetector/ObjectDetection.py
@@ -7,6 +7,12 @@ import numpy as np
 import sys
 import math
 import os
+from matplotlib import pyplot as plt
+
+'''
+Object detection code, specificallty for IR images (grayscale), but not limited to.
+The run time can be improved by updating the self.findBbox: 1. not to run on the entire image, 2. not for large different sizes of objects
+'''
 
 class ObjectDetection(unittest.TestCase):
     def setUp(self):
@@ -28,12 +34,11 @@ class ObjectDetection(unittest.TestCase):
 
     def setParameters(self):
                
-        basePath = r"/home/yossi/Documents/database/hadar" # for yossi pc
-        # basePath = r"C:\Users\User\Documents\dataBase\DSP-IP" # for Hadar's personal pc
+        # basePath = r"/home/yossi/Documents/database/hadar" # for yossi pc
+        basePath = r"C:\Users\User\Documents\dataBase\DSP-IP" # for Hadar's personal pc
 
-        # relPath = r"videos\IR_Videos\IR_AIRPLANE_002.mp4"
-        relPath = r"videos/IR_Videos/drone1_1m.mp4"
-        relPath = r"videos/IR_Videos/saved_drone1_1m_grayLevel150.mp4"
+        # relPath = r"videos/IR_Videos/output_drone1_1m.mp4"                # input path
+        relPath = r"videos/IR_Videos/drone1_1m.mp4"                         # input path
 
         outRelPath = r"videos/outputVideos/output_drone1_1m.mp4"
 
@@ -54,7 +59,8 @@ class ObjectDetection(unittest.TestCase):
 
         self.screenSizeFactor = 1 # 0.9 for my laptop smaller screen
 
-        self.frameCutFactor = 1 # to cut annotations built-in on images like FLIR and other trademarks...
+        self.frameCutFactor = 1 # to cut annotations built-in on images like the word "FLIR" and other trademarks...
+        self.cutSize        = 100 # the number of pixels to cut to eliminate any annotations 
 
         self.imageDownScaleFactor = 0.05 # for detection in images with large pixels size (low resolution)
 
@@ -73,19 +79,17 @@ class ObjectDetection(unittest.TestCase):
                 newFrame = cv2.resize(newFrame, None, fx=self.screenSizeFactor,fy=self.screenSizeFactor)
 
             if self.frameCutFactor != 1:
-                newFrame = newFrame[newFrame.shape[0] - frameHeight : frameHeight, newFrame.shape[1] - frameWidth  : frameWidth ]
+                newFrame = newFrame[newFrame.shape[0] - self.cutSize : self.cutSize, newFrame.shape[1] - self.cutSize  : self.cutSize ]
 
             frameGray = cv2.cvtColor(newFrame, cv2.COLOR_BGR2GRAY)
 
             pMax = self.findBbox(frameGray)
 
-            annotatedFrame = cv2.circle(newFrame, center=pMax, radius=max(2, 2*self.imageDownScaleFactor),color=(0,0,255), thickness=1)
+            annotatedFrame = cv2.circle(newFrame, center=pMax, radius=4 ,color=(0,0,255), thickness=2)
 
-            # annotatedFrame = cv2.circle(newFrame, center=pMax, radius=max(12, 2*self.imageDownScaleFactor),color=(5,5,5), thickness=-1)
-            # dgl = 150 # drone Gray Level
-            # annotatedFrame = cv2.circle(newFrame, center=pMax, radius=max(1, 2*self.imageDownScaleFactor),color=(dgl,dgl,dgl), thickness=-1)
+            # self.createSmallerTarget(newFrame, pMax) # for creating a new video with a different synthetic drone.
 
-            cv2.imshow('frame', annotatedFrame) 
+            cv2.imshow('frame', cv2.resize(newFrame, None, fx=0.9,fy=0.9)) 
 
             # self.outputVideo.write(newFrame)
                
@@ -96,6 +100,14 @@ class ObjectDetection(unittest.TestCase):
         cv2.destroyAllWindows() 
         self.cap.release()
         self.outputVideo.release()
+
+    #---------------------------------------------------------------------------------------------------------------------
+
+    def createSmallerTarget(self, newFrame, pMax):
+
+        cv2.circle(newFrame, center=pMax, radius=20, color=(5,5,5), thickness=-1)
+        dgl = 150 # drone Gray Level
+        cv2.circle(newFrame, center=pMax, radius=max(1, 2*self.imageDownScaleFactor),color=(dgl,dgl,dgl), thickness=-1)
 
     #---------------------------------------------------------------------------------------------------------------------
        
@@ -121,57 +133,45 @@ class ObjectDetection(unittest.TestCase):
     #---------------------------------------------------------------------------------------------------------------------
         
     def findBbox(self, frameGray):
-        median3 = cv2.blur(frameGray, ksize=(3,3))
-        frameGrayDs = cv2.resize(median3, None, fx=self.imageDownScaleFactor, fy=self.imageDownScaleFactor, interpolation=cv2.INTER_AREA)
-        ret, thresholdImage = cv2.threshold(frameGrayDs, 50, 255, cv2.THRESH_BINARY)
-        
-        cv2.imshow("frameGrayDs", cv2.resize(frameGrayDs, None, fx=1/self.imageDownScaleFactor, fy=1/self.imageDownScaleFactor, interpolation=cv2.INTER_AREA))
-        cv2.waitKey(0)
-        cv2.imshow("thresholdImage", thresholdImage)
-        cv2.waitKey(0)
-        # boxSide = int((9 * self.imageDownScaleFactor)//2)
 
-        boxSide = 1
-        # kernelSize = max(9, int(np.ceil(31 * self.imageDownScaleFactor)))
-        kernelSize = 9
-        kernel = 0*np.ones((kernelSize,kernelSize),np.uint8)
-        kernel[kernelSize//2-boxSide+1:kernelSize//2+boxSide, kernelSize//2-boxSide+1:kernelSize//2+boxSide] = 1
+        kernelSize = 31
+        max_val_max = 0
+        boxSideList = [1, 3, 5, 7] # going over different object sizes to match template with
+        for boxSide in boxSideList:
+            kernel = np.zeros((kernelSize,kernelSize),np.uint8)
+            cv2.circle(kernel, center=(kernelSize//2,kernelSize//2), radius=boxSide,color=(255,255,255), thickness=-1)
 
-        print(f"{kernel[:,kernel.shape[1]//2]=}")
-        print(f"{np.sum(kernel)=}")
+            corrMat = cv2.matchTemplate(image=frameGray, templ=kernel, method=cv2.TM_CCORR_NORMED)
 
-        corrMat = cv2.matchTemplate(image=thresholdImage, templ=kernel, method=cv2.TM_CCORR_NORMED)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(corrMat)
 
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(corrMat)
-        print(f"{max_loc=}")
+            if max_val > max_val_max: # tke the best correlation coeeficient (max_val) of all the boxSide
+                max_val_max = max_val
+                max_loc_max = max_loc
 
-        pMax = max_loc[0] + kernelSize//2, max_loc[1] + kernelSize//2
+        pMax = max_loc_max[0] + kernelSize//2, max_loc_max[1] + kernelSize//2 # correlation matrrix (corrMat) is smaller than image so adding the difference
 
-        pMax = self.getFineTune(frameGray, pMax, (frameGrayDs.shape[0], frameGrayDs.shape[1]))
+        pMax = self.getFineTune(frameGray, pMax) # get the center of objet/blob
 
         return pMax
 
     #---------------------------------------------------------------------------------------------------------------------
     
-    def getFineTune(self, frameGray, pMaxDs, originShape):
+    def getFineTune(self, frameGray, pMax):
             
-        fineTuneMatchBox = 100
+        fineTuneMatchBox = 100 # search center of blob in this area 100x100 around the object estimated location 
 
-        pMaxUpscaled = (int(pMaxDs[0] * frameGray.shape[1]/originShape[1]), int(pMaxDs[1] * frameGray.shape[0]/originShape[0]))
-        print(f"{pMaxDs=}")
-        print(f"{pMaxUpscaled=}")
+        frameGraySub = frameGray[ pMax[1]-fineTuneMatchBox//2:pMax[1]+fineTuneMatchBox//2,
+                                  pMax[0]-fineTuneMatchBox//2:pMax[0]+fineTuneMatchBox//2 ]
 
-        fineTuneSmallFrame = frameGray[ pMaxUpscaled[1]-fineTuneMatchBox//2:pMaxUpscaled[1]+fineTuneMatchBox//2,
-                                        pMaxUpscaled[0]-fineTuneMatchBox//2:pMaxUpscaled[0]+fineTuneMatchBox//2 ]
+        ret,thresh = cv2.threshold(frameGraySub,30,255,0)  
 
-        ret,thresh = cv2.threshold(fineTuneSmallFrame,150,255,0)     
-        # calculate moments of binary image
         M = cv2.moments(thresh)     
         # calculate x,y coordinate of center mji=∑x,y(array(x,y)⋅xj⋅yi), xj=x^j. yi=y^i
         cX = int(M["m10"] / M["m00"])
         cY = int(M["m01"] / M["m00"])
 
-        pMax = pMaxUpscaled[0] + cX - fineTuneMatchBox//2, pMaxUpscaled[1] + cY - fineTuneMatchBox//2
+        pMax = pMax[0] + cX - fineTuneMatchBox//2, pMax[1] + cY - fineTuneMatchBox//2 # like with the correlation matrrix, adding the difference
 
         return pMax
 
@@ -180,33 +180,3 @@ class ObjectDetection(unittest.TestCase):
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(ObjectDetection)
     unittest.TextTestRunner(verbosity=0).run(suite)
-
-
-
-
-
-
-
-
-            # numLabels, labels, stats, centroids =  cv2.connectedComponentsWithStats(image=thresholdImage, connectivity=4, ltype=cv2.CV_32S)
-	    # # x,y,w,h,area = stats[i, cv2.CC_STAT_LEFT], stats[i, cv2.CC_STAT_TOP], stats[i, cv2.CC_STAT_WIDTH], stats[i, cv2.CC_STAT_HEIGHT], stats[i, cv2.CC_STAT_AREA]
-        # if numLabels > 1:
-        #     areasList = stats[:, cv2.CC_STAT_AREA] 
-
-        #     print(f"{stats=}")
-        #     print(f"{areasList=}")
-        #     maxAreaIdx = np.argmax(areasList[1:]) # index 0 is for background with the maximal area
-        #     print(f"{maxAreaIdx=}")
-        #     self.cX,self.cY = centroids[1:][maxAreaIdx]
-        #     print(f"{self.cX,self.cY}")
-
-        #     # finalImage = morphImage.copy()
-    
-        #     # (yi, xi) = np.unravel_index(finalImage.argmax(), finalImage.shape)
-
-        #     # print(f"{xi=}, {yi=}")
-        #     # x0, x1 = xi-self.bboxWidth,  xi+self.bboxWidth
-        #     # y0, y1 = yi-self.bboxHeight, yi+self.bboxHeight
-
-        #     # cv2.rectangle(img=frame, pt1=(x0, y0), pt2=(x1, y1), color=(255,0,0), thickness=1)   
-        # cv2.circle(img=frame, center=(int(self.cX), int(self.cY)), radius=1, color=(0,0,255), thickness=-1)   
